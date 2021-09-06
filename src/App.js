bb8a789514705799fc91ea19cc29b336ae92b166
qi.wu <qi.wu@coinbase.com>
2021-09-05 20:10:48 -0700

    Add enter button

--- src/App.js
+++ src/App.js
@@ -7,20 +7,43 @@ import lottery from './lottery';
 class App extends React.Component {
   // Initialize state
   state = {
-    manager: ""
+    balance: "",
+    manager: "",
+    players: [],
+    value: ""
   };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager })
+    this.setState({ balance, manager, players })
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
+
+        <hr />
+
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
