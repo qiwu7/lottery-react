b1b52426c2c185af02908525137d93bd146b922a
qi.wu <qi.wu@coinbase.com>
2021-09-05 20:00:23 -0700

    Render lottery contract manager

---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from "./web3";
+
+const ABI = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+const CONTRACT_ADDRESS = "0xD34e864B3e29F379D89AAc26a75D2B9CFC64b7D7";
+const lottery = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
+
+export default lottery;
